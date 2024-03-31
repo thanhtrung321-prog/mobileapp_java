f1b9d84dc99484de5ac4aaccf825081f3a29b383
thanhtrung321-prog <tvo30879@hmail.com>
2024-03-31 13:06:24 +0700

    thành công chuyển tới login Admin

--- app/src/main/java/com/example/vothanhtrung_shop/Start_Admin.java
+++ app/src/main/java/com/example/vothanhtrung_shop/Start_Admin.java
@@ -2,7 +2,10 @@ package com.example.vothanhtrung_shop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Start_Admin extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Start_Admin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_admin);
+        Button ButttonloginAdmin=findViewById(R.id.ButttonloginAdmin);
+        ButttonloginAdmin.setOnClickListener(new View.OnClickListener() {
+                                                 @Override
+                                                 public void onClick(View v) {
+                                                     Intent intent = new Intent(Start_Admin.this,LoginAdminActivity.class);
+                                                    startActivity(intent);
+                                                 }
+                                             }
+        );
     }
 }
