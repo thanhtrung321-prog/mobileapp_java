e17100a82f26f5077bf242965163ee2d6348b941
thanhtrung321-prog <tvo30879@hmail.com>
2024-04-03 02:10:57 +0700

    còn giao diện giỏ hàng chưa làm

--- app/src/main/java/com/example/vothanhtrung_shop/CreateUserAdminActivity.java
+++ app/src/main/java/com/example/vothanhtrung_shop/CreateUserAdminActivity.java
@@ -3,21 +3,22 @@ package com.example.vothanhtrung_shop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class AdminprofileActivity extends AppCompatActivity {
-    protected ImageView pagehome;
+public class CreateUserAdminActivity extends AppCompatActivity {
+    protected  ImageView pagehomeadduser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_adminprofile);
-        pagehome=findViewById(R.id.page_home_admin);
-        pagehome.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_create_user_admin);
+        pagehomeadduser=findViewById(R.id.page_home_admin_adduser);
+        pagehomeadduser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(AdminprofileActivity.this,HomeAdminActivity.class);
+                Intent intent=new Intent(CreateUserAdminActivity.this,HomeAdminActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/vothanhtrung_shop/HomeAdminActivity.java
+++ app/src/main/java/com/example/vothanhtrung_shop/HomeAdminActivity.java
@@ -5,13 +5,17 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Toast;
 
 public class HomeAdminActivity extends AppCompatActivity {
 
     private ConstraintLayout addproduct;// Khai báo biến addproduct ở đây
     protected ConstraintLayout allproduct;
     protected  ConstraintLayout profileadmin;
+    protected  ConstraintLayout adduseradmin;
+    protected ConstraintLayout logouthome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,36 @@ public class HomeAdminActivity extends AppCompatActivity {
         addproduct = findViewById(R.id.Addproduct);
         allproduct=findViewById(R.id.Allproductview);
         profileadmin=findViewById(R.id.profileAdmin);
+        adduseradmin=findViewById(R.id.addUserAdmin);
+        logouthome=findViewById(R.id.logoutadminHome);
+        logouthome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Hiển thị thông báo đăng xuất thành công
+                Toast.makeText(HomeAdminActivity.this, "Đăng xuất thành công", Toast.LENGTH_SHORT).show();
+
+                // Đặt thời gian chờ trước khi chuyển sang MainActivity
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Tạo Intent để chuyển đến MainActivity
+                        Intent intent = new Intent(HomeAdminActivity.this, MainActivity.class);
+                        startActivity(intent); // Chuyển sang MainActivity
+                        finish(); // Kết thúc Activity hiện tại
+                    }
+                }, 600); // Đợi 1 giây trước khi chuyển sang MainActivity
+            }
+        });
+
+
+
+        adduseradmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(HomeAdminActivity.this,CreateUserAdminActivity.class);
+                startActivity(intent);
+            }
+        });
         profileadmin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
